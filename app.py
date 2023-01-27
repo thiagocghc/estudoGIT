c1ad78ba5c7bdd86e9e24b4fc26ae7010890c02c
Thiago Almeida <thiagoalmeida@live.com>
2023-01-27 01:50:03 -0300

    arquivo atualizado na branch do nathan

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-'''
 app1 = "app1"
 print(app1)
-'''
+
+#atualizando o arquivo na BRANCH DO NATHANNN
+
 
 x=10
 y=12
 z = x*y
 print(z)
 
-#testando
+#atualizando o arquivo na MASTER
+#atualizando o arquivo na BRANCH DO NATHANNN
